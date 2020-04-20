4fe761251ea3d07250c4d9a5715c20035fec3958
Vsevolod <Vsevolod_Popov@epam.com>
2020-04-20 14:21:53 +0300

    Added necessary functionality to controllers with changes

--- MeetingSimulator.Api/Program.cs
+++ MeetingSimulator.Api/Program.cs
@@ -14,22 +14,6 @@ namespace MeetingSimulator.Api
     {
         public static void Main(string[] args)
         {
-            //MeetingSimulatorDbContext meetingSimulatorDbContext = new MeetingSimulatorDbContext();
-            //IMemberRepository memberRepository = new MemberRepository(meetingSimulatorDbContext);
-            //MemberServices memberServices = new MemberServices(memberRepository);
-            //MeetingRepository meetingRepository = new MeetingRepository(meetingSimulatorDbContext);
-            //MeetingService meetingService = new MeetingService(meetingRepository);
-            //MemberMeetingRepository memberMeetingRepository = new MemberMeetingRepository(meetingSimulatorDbContext);
-            //MemberMeetingService memberMeetingService = new MemberMeetingService(memberMeetingRepository);
-            //var a = memberRepository.ContainsId(4);
-            //var b = memberMeetingRepository.ContainsMemberId(4);
-           //memberMeetingService.AddMembersToTheMeeting(3, 4);
-
-            //foreach (var item in some)
-            //{
-            //    Console.WriteLine(item.MeetingName, item.Place, item.Surname);
-            //}
-
             CreateHostBuilder(args).Build().Run();
         }
 
--- MeetingSimulator.Api/Startup.cs
+++ MeetingSimulator.Api/Startup.cs
@@ -16,6 +16,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+
 namespace MeetingSimulator.Api
 {
     public class Startup
@@ -27,7 +28,6 @@ namespace MeetingSimulator.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<MeetingSimulatorDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DbContext")));
@@ -38,7 +38,6 @@ namespace MeetingSimulator.Api
             services.AddTransient<IMemberMeetingRepository, MemberMeetingRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
